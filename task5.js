02d8681deb0e9b255f9763818d7d28834a3e038b
Harshit2026 <choubeyharshit@gmail.com>
2024-07-08 00:24:14 +0530

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,44 +1,38 @@
 function getScroll(){
     let y = scrollY
     let colors = ["#fff" , "#cf404d" ]
-    let links = [document.getElementById('hme').style.color , document.getElementById('abt').style.color , document.getElementById('glry').style.color,  
-                 document.getElementById('tact').style.color]
+    let links = [document.getElementById('hme') , document.getElementById('abt') , document.getElementById('glry'),  
+                 document.getElementById('tact')]
     if(y >=0 && y<238) {
-        links[0] = colors[1]
-        links[1] = colors[0]
-        links[2] = colors[0]
-        links[3] = colors[0]
+        links[0].style.color = colors[1]
+        links[1].style.color = colors[0]
+        links[2].style.color = colors[0]
+        links[3].style.color = colors[0]
 
     }
         
     else if(y > 238 && y <=960){
-        links[0] = colors[0]
-        links[1] = colors[1]
-        links[2] = colors[0]
-        links[3] = colors[0]
+        links[0].style.color = colors[0]
+        links[1].style.color = colors[1]
+        links[2].style.color = colors[0]
+        links[3].style.color = colors[0]
 
 
 
     }
     else if(y > 960 && y <= 1618){
-        links[0] = colors[0]
-        links[1] = colors[0]
-        links[2] = colors[1]
-        links[3] = colors[0]
+        links[0].style.color = colors[0]
+        links[1].style.color = colors[0]
+        links[2].style.color = colors[1]
+        links[3].style.color = colors[0]
 
 
     }
     else {
-        links[0] = colors[0]
-        links[1] = colors[0]
-        links[2] = colors[0]
-        links[3] = colors[1]
+        links[0].style.color = colors[0]
+        links[1].style.color = colors[0]
+        links[2].style.color = colors[0]
+        links[3].style.color = colors[1]
     }
 }
 setInterval(getScroll , 250);
-        
-    }
-        
-}
-setInterval(getScroll,250);   //960 //1618
-
